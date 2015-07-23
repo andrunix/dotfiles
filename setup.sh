32afd875887125c0f0633319cc4bd37241a27eb7
Andrew Pierce <andrunix@gmail.com>
2015-07-23 10:47:32 -0400

    Fix setup.sh for Linux compatibility.

--- setup.sh
+++ setup.sh
@@ -4,19 +4,19 @@ bkupdir=$HOME/.dotfilesbackup/`date +"%Y-%m-%d"`
 
 if [ ! -d ${bkupdir} ]; then
   echo "Creating backup directory, ${bkupdir}"
-  mkdir ${bkupdir}
+  mkdir -p ${bkupdir}
 fi
 
 if [ -f $HOME/.tmux.conf ]; then
   echo "Moving tmux conf to ${bkupdir}"
   mv $HOME/.tmux.conf $bkupdir
 fi
-ln -shf `pwd`/tmux/.tmux.conf $HOME/.tmux.conf
+ln -sf `pwd`/tmux/.tmux.conf $HOME/.tmux.conf
 
 if [ -f $HOME/.zshrc ]; then
   echo "Moving zshrc to ${bkupdir}"
   mv $HOME/.zshrc $bkupdir
 fi
-ln -s `pwd`/zsh/.zshrc $HOME/.zshrc
+ln -sf `pwd`/zsh/.zshrc $HOME/.zshrc
 
 
